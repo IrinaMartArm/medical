c9c71cbda110aeb88ea38fa69eb616084f0d6f9d
Ирина Мартоян <surenira@icloud.com>
2024-01-27 16:13:53 +0400

    settings for seo

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React, {Suspense} from 'react';
+import { hydrate, render } from "react-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
@@ -8,21 +9,33 @@ import './config/i18n'
 import {BrowserRouter} from "react-router-dom";
 
 
-const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
-);
-root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<Suspense fallback={<div></div>}>
-				<GlobalStyle/>
-				<App/>
-			</Suspense>
-		</BrowserRouter>
-	</React.StrictMode>
-);
+// const root = ReactDOM.createRoot(
+// 	document.getElementById('root') as HTMLElement
+// );
+// root.render(
+// 	<React.StrictMode>
+// 		<BrowserRouter>
+// 			<Suspense fallback={<div></div>}>
+// 				<GlobalStyle/>
+// 				<App/>
+// 			</Suspense>
+// 		</BrowserRouter>
+// 	</React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes()) {
+	hydrate(<BrowserRouter>
+		<Suspense fallback={<div></div>}>
+			<GlobalStyle/>
+			<App/>
+		</Suspense>
+	</BrowserRouter>, rootElement);
+} else {
+	render(<BrowserRouter>
+		<Suspense fallback={<div></div>}>
+			<GlobalStyle/>
+			<App/>
+		</Suspense>
+	</BrowserRouter>, rootElement);
+}
