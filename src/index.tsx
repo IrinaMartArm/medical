43b9b9c40cf274b8359ff12d66ae45e4d6043595
Ирина Мартоян <surenira@icloud.com>
2024-01-27 16:30:18 +0400

    settings for seo

--- src/index.tsx
+++ src/index.tsx
@@ -1,41 +1,41 @@
 import React, {Suspense} from 'react';
-import { hydrate, render } from "react-dom";
+// import { hydrate, render } from "react-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import {GlobalStyle} from "./style/GlobalStyle";
 import './config/i18n'
 import {BrowserRouter} from "react-router-dom";
 
 
-// const root = ReactDOM.createRoot(
-// 	document.getElementById('root') as HTMLElement
-// );
-// root.render(
-// 	<React.StrictMode>
-// 		<BrowserRouter>
-// 			<Suspense fallback={<div></div>}>
-// 				<GlobalStyle/>
-// 				<App/>
-// 			</Suspense>
-// 		</BrowserRouter>
-// 	</React.StrictMode>
-// );
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
+root.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Suspense fallback={<div></div>}>
+				<GlobalStyle/>
+				<App/>
+			</Suspense>
+		</BrowserRouter>
+	</React.StrictMode>
+);
 
-const rootElement = document.getElementById("root");
-if (rootElement?.hasChildNodes()) {
-	hydrate(<BrowserRouter>
-		<Suspense fallback={<div></div>}>
-			<GlobalStyle/>
-			<App/>
-		</Suspense>
-	</BrowserRouter>, rootElement);
-} else {
-	render(<BrowserRouter>
-		<Suspense fallback={<div></div>}>
-			<GlobalStyle/>
-			<App/>
-		</Suspense>
-	</BrowserRouter>, rootElement);
-}
+// const rootElement = document.getElementById("root");
+// if (rootElement?.hasChildNodes()) {
+// 	hydrate(<BrowserRouter>
+// 		<Suspense fallback={<div></div>}>
+// 			<GlobalStyle/>
+// 			<App/>
+// 		</Suspense>
+// 	</BrowserRouter>, rootElement);
+// } else {
+// 	render(<BrowserRouter>
+// 		<Suspense fallback={<div></div>}>
+// 			<GlobalStyle/>
+// 			<App/>
+// 		</Suspense>
+// 	</BrowserRouter>, rootElement);
+// }
